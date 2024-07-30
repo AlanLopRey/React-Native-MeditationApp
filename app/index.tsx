6ee7c6bfe174da84971affa7b41ddcc33e580248
Alan Lopez <99952929+AlanLopRey@users.noreply.github.com>
2024-07-30 00:59:37 -0600

    Get done with the meditate screen and the affirmation screen

--- app/index.tsx
+++ app/index.tsx
@@ -8,51 +8,47 @@ import {
 } from "react-native";
 
 import { StatusBar } from "expo-status-bar";
-import React, { Component } from "react";
 import { LinearGradient } from "expo-linear-gradient";
 
 import BeachImage from "@/assets/meditation-images/beach.webp";
 import CustomButton from "@/components/CustomButton";
+import { useRouter } from "expo-router";
+import AppGradient from "@/components/AppGradient";
+export function App() {
+  const router = useRouter();
 
-export class App extends Component {
-  render() {
-    return (
-      <View className="flex-1">
-        <ImageBackground
-          source={BeachImage}
-          resizeMode="cover"
-          className="flex-1"
-        >
-          <LinearGradient
-            className="flex-1"
-            colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}
-          >
-            <View className="flex-1 mx-5 mb-12 justify-between">
-              <View>
-                <Text
-                  style={styles.safe_view}
-                  className="text-center text-white font-bold text-4xl"
-                >
-                  Meditation App
-                </Text>
-                <Text className="text-center text-white text-2xl mt-3">
-                  Simplifying Meditation for everyone
-                </Text>
-              </View>
-
-              <View>
-                <CustomButton
-                  onPress={() => console.log("tap")}
-                  title="Get Started"
-                />
-              </View>
+  return (
+    <View className="flex-1">
+      <ImageBackground
+        source={BeachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <AppGradient colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}>
+          <View className="flex-1 px-1 justify-between">
+            <View>
+              <Text
+                style={styles.safe_view}
+                className="text-center text-white font-bold text-4xl"
+              >
+                Meditation App
+              </Text>
+              <Text className="text-center text-white text-2xl mt-3">
+                Simplifying Meditation for everyone
+              </Text>
             </View>
-            <StatusBar translucent={true} style="light" />
-          </LinearGradient>
-        </ImageBackground>
-      </View>
-    );
-  }
+            <View>
+              <CustomButton
+                onPress={() => router.push("/nature-meditate")}
+                title="Get Started"
+              />
+            </View>
+          </View>
+          <StatusBar translucent={true} style="light" />
+        </AppGradient>
+      </ImageBackground>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
